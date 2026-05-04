8624b48ae9f500fec7c9eedcf95632b2765e8628
Astha Issrani <asthaissrani711@gmail.com>
2026-05-04 16:54:54 +0530

    fixed a bug

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,9 @@ app.get('/api/health', (req, res) => {
 // Serve React frontend in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-  });
+  app.get('/{*splat}', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 }
 
 // Global error handler
